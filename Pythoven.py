c4baefa906aad9be534e82527c91552b0fca0f13
tehdog <tehdog22@gmail.com>
2012-09-08 13:58:49 +0200

    current status

---
+++ Pythoven.py
@@ -0,0 +1,436 @@
+#!/usr/bin/python
+# Pythoven 2 / tehdog
+# Writes music
+
+# Notes stored as a list of tuples of the form (start, step)
+# The first entry in a track is the track's length
+# time in measure is on a scale of sixteenth notes (0x0 -> 0xF)
+# measures are therefore measured hexadecimally as well.
+# This convention is not required, but is recommended for anything
+# written in a binary time (4/4, 2/4, etc...)
+
+# note is an integer representing the number of half-steps off the
+# base note
+
+'''
+@author: darkspork,tehdog
+'''
+
+import random, os, errno, sys, Waves
+from random import randint      # Used in wrand
+
+def replaceprint(s):
+    sys.stdout.write("\r"+str(s)+" "*10)
+    sys.stdout.flush()
+
+curprogress=0   
+def startprogress(s):
+    sys.stdout.write('\r'+(len(s)+30)*' '+']')
+    sys.stdout.write('\r'+s+'[')
+    sys.stdout.flush()
+    curprogress=0
+def updateprogress(prog):
+    if int(prog*30)>curprogress:
+        curprogress=int(prog*30)
+        sys.stdout.write('=')
+        sys.stdout.flush()
+    
+def makeScale(rawScale):
+    """makeScale
+        rawScale - a raw version of the scale to use
+        returns a scale: a tuple such that the
+            first item is a list of acceptable offsets,
+            and the second is the size of the scale"""
+    note = 0
+    li = []
+    for n in rawScale:
+        li.append(note)
+        note += n
+    return (li, sum(rawScale))
+
+# The scale of notes, represented in text form
+NOTES = ('C','C#','D','D#','E','F','F#','G','G#','A','A#','B')
+
+
+# Various scales
+MAJOR          = makeScale((2,2,1,2,2,2,1))
+NATURAL_MINOR  = makeScale((2,1,2,2,1,2,2))
+MELODIC_MINOR  = makeScale((2,1,2,2,2,2,1))
+HARMONIC_MINOR = makeScale((2,1,2,2,1,3,1))
+WHOLE_TONE     = makeScale((2,2,2,2,2,2))
+PENTATONIC     = makeScale((2,3,2,2))
+CHROMATIC      = makeScale((1,1,1,1,1,1,1,1,1,1,1,1))
+
+# Notes and quality
+# Basically, measure the amount of dissonance, in half-steps
+# Higher number = worse sound
+# If dissonance becomes too high, then remake this note
+# dissonance = melodic dissonance + average harmonic dissonance
+
+# Half-steps:              -C  -B  -A  -9  -8  -7  -6  -5  -4  -3  -2  -1   0   1   2   3   4   5   6   7   8   9   A   B   C
+# In the key of C:         C   C#  D   D#  E   F   F#  G   G#  A   A#  B   C   C#  D   D#  E   F   F#  G   G#  A   A#  B   C
+
+MELODIC_INTERVAL = ((-12,5),(-11,6),(-10,4),( -9,3),( -8,2),( -7,3),
+                    ( -6,4),( -5,1),( -4,1),( -3,2),( -2,2),( -1,3),
+                    (  0,0),
+                    (  1,3),(  2,2),(  3,2),(  4,1),(  5,1),(  6,4),
+                    (  7,3),(  8,2),(  9,3),( 10,4),( 11,6),( 12,5))
+
+# Half-steps:               0   1   2   3   4   5   6   7   8   9   A   B
+# In the key of C:         C   C#  D   D#  E   F   F#  G   G#  A   A#  B
+HARMONIC_INTERVAL      = (  0, 10,  8,  3,  2,  1,  8,  1,  2,  3,  7,  9)
+
+def getLastNote(track, position):
+    """getLastNote
+        track    - a list of notes
+        position - the position (int) to start from
+        return a tuple (time, note)
+            of the most recent note from position"""
+    return [(time, note) for (time, note) in track[1:] if time <= position][-1]
+
+def noteString(note, key='C', padded = False):
+    """noteString
+        note - a number indicating the halfstep offset from the key
+        key - the key to print the track in as a string (i.e. 'F#')
+        return a string representation of a note"""
+    i = note + NOTES.index(key)
+    g = len(NOTES)
+    n = 0
+    s = ''
+    while i < 0:
+        i += g
+        n -= 1
+    while i >= g:
+        i -= g
+        n += 1
+    if n > 0:
+        s = "+%d%s"%(n,NOTES[i])
+    elif n < 0:
+        s = "%d%s"%(n,NOTES[i])
+    else:
+        s = "%s%s"%(padded and '  ' or '',NOTES[i])
+    if padded:
+        return s.ljust(4)
+    return s
+
+def trackString(track, key='C', measure=0):
+    """trackString
+        track   - a list of notes
+        key     - the key to print the track in as a string (i.e. 'F#')
+        measure - length of measure
+                    note: if given, makes output easier to read
+        return a string representation of a track"""
+    #return "".join(["%s%s"%("".ljust(2*time),noteString(note, key).ljust(2)) for (time, note) in track]).replace(" ", "-")
+    if measure:
+        i  = 0
+        li = []
+        for (time, note) in track[1:]:
+            while i < time:
+                if not i % measure: # on measure break
+                    li.append('\n')
+                li.append('    ')
+                i += 1
+            if not i % measure: # on measure break
+                li.append('\n')
+            li.append(noteString(note, key, True))
+            i += 1
+        return "".join(li)
+    return ", ".join(["0x%X:%s"%(time,noteString(note, key, False)) for (time, note) in track[1:]])
+
+def wrap(li, i):
+    """wrap
+        li - a list
+        i  - an index
+        return li[i] (if i is out of range, wrap around)"""
+    l = len(li)
+    j = i
+    while j < 0:
+        j += l
+    while j >= l:
+        j -= l
+    return li[j]
+
+def inScale(note, scale=MAJOR):
+    """inScale
+        note  - a numeric key offset note
+        scale - scale to investigate
+        size  - # of half steps in scale
+        return true if note is in scale"""
+    n = note
+    while n < 0:
+        n += scale[1]
+    while n > scale[1]:
+        n -= scale[1]
+    return n in scale[0]
+
+def wrand(d):
+    """Find a value with a weighted average
+        d - a wrand dictionary
+        returns a random value in d"""
+    i = randint(0, d['max'])
+    while i>=0 and not d.has_key(i):
+        i -= 1
+    return d[i]
+
+def melodic2wrand(melodic=MELODIC_INTERVAL):
+    """melodic2wrand
+        melodic - a melodic interval to use"""
+    d = {}
+    weight = 0
+    for (interval, freq) in melodic:
+        d[weight] = interval
+        weight += 10-freq
+    d['max'] = weight
+    return d
+
+def mktimes(measure, beat, sync, length):
+    """mktimes
+        measure  - number of beats in a measure
+        beat     - default beats per note (must be < measure, should be divisible by measure)
+        sync     - % chance that each beat will be split
+        length   - how many measures to create
+        returns a list of times that a note should appear"""
+    li = []
+    for i in range(0,length*measure//beat):
+        li.append(i * beat)
+        if (randint(0,100) < sync):
+            li.append(i * beat + beat // 2)
+    return li
+
+def loop(track, length):
+    """loop
+        track - the track to loop
+        length - the number of ticks to loop to
+        returns a new track"""
+    newtrack=[length]
+    i = 0;
+    while i < length:
+        newtrack.extend([(time+i,note) for (time,note) in track[1:]])
+        i += track[0]
+    while newtrack[-1][0] > length:
+        del(newtrack[-1])
+    return newtrack
+
+def avgdissonance(sheet, time, mainnote, harmonic):
+    """avgdissonance
+        sheet    - a music sheet
+        time     - a time to count dissonance at
+        mainnote - the note to check dissonance against
+        harmonic - a harmonic interval to use
+        returns the average dissonance between
+            the last track's last note and the rest"""
+    notes = [getLastNote(track, time) for track in sheet]
+    return sum([wrap(harmonic,abs(n[1]-mainnote)) for n in notes]) // len(sheet)
+
+def shift(track, halfsteps):
+    """shift
+        track     - the track to shift
+        halfsteps - number of half-steps (positive or negative) to shift the track
+        returns None, shifts track"""
+    for i in range(1, len(track)):
+        note=track[i]
+        track[i] = (note[0],note[1]+halfsteps)
+
+def compose(measure=16, beat=4, sync=14, length=1, stray=10, scale=MAJOR, melodic=MELODIC_INTERVAL, seed=None, offset=0):
+    """compose
+        measure  - number of beats in a measure
+        beat     - default beats per note (must be < measure, should be divisible by measure)
+        sync     - % chance that each beat will be split
+        length   - how many measures to create
+        stray    - how many half-steps the notes are allowed to stray from 0
+        scale    - a musical scale to use
+        melodic  - melodic interval frequency
+        seed     - random seed
+        returns a track"""
+    random.seed(seed) # initialize the random
+    mwrand = melodic2wrand(melodic)
+    track = [length*measure,(0,0)]
+    times = mktimes(measure, beat, sync, length)
+    del(times[0])
+    for i in times:
+        note = track[-1][1] + wrand(mwrand) # because the last note in the track will always be the prev.
+        while note > stray or note < -stray or not inScale(note,scale):
+            note = track[-1][1] + wrand(mwrand) # choose next note as a melodic of the previous
+        track.append((i, note))
+    if offset!=0 :shift(track,offset)
+    return track
+
+def counterpoint(sheet, start=0, measure=16, beat=4, sync=27, length=1, stray=7, dissonance=3,
+                 scale=MAJOR, melodic=MELODIC_INTERVAL, harmonic=HARMONIC_INTERVAL, seed=None):
+    """compose
+        sheet      - a list of other tracks in the song
+        start      - a note (offset) to begin on
+        measure    - number of beats in a measure
+        beat       - default beats per note (must be < measure, should be divisible by measure)
+        sync       - % chance that each beat will be split
+        length     - how many measures to create. Note: all tracks will be looped to appropriate
+                        length before calculation
+        stray      - how many half-steps the notes are allowed to stray from the first
+        dissonance - maximum average dissonance allowed per note
+        scale      - a musical scale to use
+        melodic    - melodic interval frequency
+        harmonic   - harmonic dissonance
+        seed       - random seed
+        returns None, adds new track to sheet"""
+    random.seed(seed) # initialize the random
+    mwrand = melodic2wrand(melodic)
+    track = [length*measure,(0,start)]
+    tsheet = [loop(t, measure*length) for t in sheet] # create a temporary bastardized loop version
+    times = mktimes(measure, beat, sync, length)
+    highstray = start + stray
+    lowstray  = start - stray
+    del(times[0])
+    for i in times:
+        note = track[-1][1] + wrand(mwrand)
+        while note > highstray or note < lowstray or not inScale(note,scale) or avgdissonance(tsheet,i,note,harmonic) > dissonance:
+            note = track[-1][1] + wrand(mwrand)
+        track.append((i, note))
+    sheet.append(track)
+
+def bass(scale=MAJOR, length=4):
+    track=compose(beat=16,sync=0,length=length,scale=scale)
+    shift(track, -12)
+    return track
+
+def notbass(scale=MAJOR, length=4):
+    track=compose(beat=16,sync=0,length=length,scale=scale)
+    shift(track, +12)
+    return track
+    
+def rlen(e):
+    l=0
+    if isinstance(e,(list,tuple)):
+        for i in e: l+=rlen(i)
+        return l
+    return 1
+
+def sing(sheet, key='C', ticktime=125, instruments = (), filename='./test.wav'):
+    """sing
+        sheet       - a music sheet to sing
+        key         - the key to sing in
+        ticktime    - the time each tick in the
+                        sheet should take, in milliseconds.
+                        The default 125 with 16-tick measures
+                        in 4/4 time will result in
+                        120 beats per minute
+        instruments - a list of instruments to use
+        filename    - the name of the wav file to make
+        return None, create a wav file from the sheet"""
+    from Waves import initArray
+    
+    print('Calculating track length...',)
+    length = max([track[0] for track in sheet])
+    replaceprint('\rFile will be approx. %d seconds' % (length*ticktime//1000))
+    print('Looping tracks...',)
+    loopedsheet=[loop(track, length) for track in sheet]
+    # Convert the sheet from absolute times to relative times, and
+    # relative notes to MIDI style absolute notes
+    replaceprint('Calculating cues...')
+    cues = []
+    offset = NOTES.index(key) + 48 # Middle C is MIDI note #48
+    for track in loopedsheet:
+        cuedtrack = track[1:]
+        cuedtrack.append((track[0],0))
+        cues.append([(cuedtrack[i+1][0] - cuedtrack[i][0], cuedtrack[i][1] + offset) for i in range(0,len(cuedtrack)-1)])
+    # cues now contains the sheet in a usable format
+    startprogress('Generating waves: ')
+    notecount=rlen(cues)//2
+    percent=0
+    progress=0
+    waves = []
+    if not instruments:
+        instruments=[Waves.DEFAULT_INSTRUMENT]*len(cues)
+    vol=1.0/len(cues)
+    for track in cues:
+        trackwave=initArray()
+        instrument=instruments.pop(0)
+        for (duration, note) in track:
+            trackwave.extend(Waves.waveGen(note,duration*ticktime,instrument,vol))
+            progress+=1
+            if(int(30*progress/notecount)>percent):
+                percent=int(30*progress/notecount)
+                sys.stdout.write("=")
+                sys.stdout.flush()
+            #vprint('Generating waves...'+str(100*progress/notecount)+"%")
+        waves.append(trackwave)
+    #wtf: waves = [divide(''.join([waveGenII(freq[note],duration*ticktime,instrumentscpy) for (duration, note) in track]), len(cues)) for track in cues]
+    replaceprint('Creating mixdown...'+' '*30)
+    wave = reduce(Waves.mergeWaves, waves[1:], waves[0])
+    replaceprint('Writing file...')
+    Waves.makeWavFile(wave, filename)
+    replaceprint('Synth complete!')
+
+# removing this for now: def waveGenII(freq, length, instruments):
+
+
+def mkdirp(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST: pass
+        else: raise
+
+
+def makeSong(instrument, songname=''):
+    if not songname: songname=randomname()
+    print 'Seed and trackname: '+ songname
+    # okay so I'm going to try something here:
+    # first I will generate a "theme" with the length of two measures
+    theme = compose(length=2,seed=songname)
+    # and put it in the sheet
+    sheet =  [theme]
+    # this theme will now get a few minor variations, but be repeated through 10 measures:
+    counterpoint(sheet,dissonance=1, beat=16, length=10,seed=songname+"bass")
+    # which will be our bass track
+    basstrack=sheet[1]
+    shift(basstrack,-12)
+    sheet.pop(0)
+    counterpoint(sheet,beat=2,dissonance=3, length=20,seed=songname+"melody")
+    # todo: more stuff here
+
+    dirname='output'
+    mkdirp(dirname)
+    wavname = dirname+"/Pythoven - %s.wav" % songname
+    mp3name = dirname+"/mp3/Pythoven - %s.mp3" % songname
+    
+    sing(sheet, key='C', ticktime=125, instruments = [instrument]*len(sheet), filename=wavname)
+    print "WAV output to: \""+wavname+"\""
+    
+    mkdirp(dirname+"/mp3")
+    import subprocess
+    hasffmpeg = subprocess.call(["which","ffmpeg"],stdout=subprocess.PIPE)==0
+    if hasffmpeg:
+        cmdline=["ffmpeg", "-loglevel","error"]
+        cmdline+=["-i",wavname,"-ab","128k"]
+        cmdline+=["-metadata","title="+songname,"-metadata","artist=Pythoven 2","-metadata","album="+instrument.capitalize()]
+        cmdline+=[mp3name]
+        mp3success=subprocess.call(cmdline)==0
+        if mp3success: print "MP3 output to:\t\""+mp3name+"\""
+        else: print "MP3 output failed"
+    else:
+        print "ffmpeg not found, no mp3 output"
+
+############################## MAIN #####################################
+from RandomName import randomname
+from datetime import datetime
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv)<2:
+            print 'Usage: Pythoven.py <instrument> [<seed>]'
+            from Waves import INSTRUMENTS
+            print 'Valid instruments are '+str(', '.join(INSTRUMENTS.keys()))
+            print '<seed> is normally a randomly generated songname'
+            exit()
+        instrument=sys.argv[1]
+        songname=''
+        if len(sys.argv)>2: songname=sys.argv[2]
+        
+    
+        starttime=datetime.now()
+        makeSong(instrument,songname)
+        print "Generation took "+str(round((datetime.now()-starttime).total_seconds(),3))+"s"
+        
+    except KeyboardInterrupt:
+        print "\nInterrupted by user"
+        pass
---
+++ Waves.py
@@ -0,0 +1,142 @@
+#needs patched wave:
+import patchedwavelib as wave
+import random,array,math
+MAX = (1<<15)-1 # maximum short value, 32767
+SAMPLE_RATE = 44100
+DEFAULT_INSTRUMENT='square'
+
+cache=dict()
+
+'''Frequencies of notes
+           C        C#       D        D#       E        F        F#       G        G#       A        A#       B
+          16.35,   17.32,   18.35,   19.45,   20.60,   21.83,   23.12,   24.50,   25.96,   27.50,   29.14,   30.87, # 0
+          32.70,   34.65,   36.71,   38.89,   41.20,   43.65,   46.25,   49.00,   51.91,   55.00,   58.27,   61.74, # 1
+          65.41,   69.30,   73.42,   77.78,   82.41,   87.31,   92.50,   98.00,  103.83,  110.00,  116.54,  123.47, # 2
+         130.81,  138.59,  146.83,  155.56,  164.81,  174.61,  185.00,  196.00,  207.65,  220.00,  233.08,  246.94, # 3
+         261.63,  277.18,  293.66,  311.13,  329.63,  349.23,  369.99,  392.00,  415.30,  440.00,  466.16,  493.88, # 4 (Middle C)
+         523.25,  554.37,  587.33,  622.25,  659.26,  698.46,  739.99,  783.99,  830.61,  880.00,  932.33,  987.77, # 5
+        1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, # 6
+        2093.00, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, # 7
+        4186.01, 4434.92, 4698.64, 4978.03)'''
+FREQS=[440*2**(x/12.0) for x in range(-57,43)]        
+
+def squareWave(freq, sampleCount, vol):
+    """sineWave
+        freq - the frequency of the wave
+        length - the length to play the wave for in milliseconds
+        vol - the volume between 0 and 1"""
+    innermult=2*freq/SAMPLE_RATE
+    outermult=int(MAX*vol)
+    values = initArray(sampleCount)
+    for i in xrange(0, sampleCount):
+        values[i]=(int(i*innermult)%2*2-1)*outermult    
+    return values
+
+def guitarWave(freq, sampleCount, vol, damping=0.996):
+    n = int(SAMPLE_RATE//freq) # noise loop filter length
+    zn = array.array('h',[int((random.random()*2-1)*MAX*vol) for i in xrange(0, n)]) # white noise (in real array for speed)
+    
+    values = initArray(sampleCount)
+    for i in xrange(0, sampleCount):
+        values[i] = zn[i % n] # read sample from current noise
+        zn[i % n] = int((
+                     zn[ i  % n]*2 +
+                     zn[(i+1) % n] +
+                     zn[(i+2) % n]
+                    ) * 0.25 * damping) # an average filter (replacement for lowpass)
+    return values
+   
+
+def sineWave(freq, sampleCount, vol):
+    """sineWave
+        freq - the frequency of the wave
+        length - the length to play the wave for in milliseconds
+        vol - the volume between 0 and 1"""
+    # using the wavelength here would round it a lot, so we calculate 20 wavelengths
+    # for example, instead of 440hz we actually use ~439.9
+    calclength=min(sampleCount,int(round(20 * SAMPLE_RATE/freq)))
+    innermult = 2 * math.pi * 20 / calclength
+    outermult = MAX*vol
+    values = initArray(sampleCount)
+    for i in xrange(0, calclength):
+        values[i]=int(math.sin(i*innermult)*outermult)    
+    for i in xrange(calclength, sampleCount):
+        values[i]=values[i%calclength]
+    
+    smoothLength = min(sampleCount//2,int(SAMPLE_RATE * 0.005))#smooth five seconds at the wave ends to remove cracking
+    for i in xrange(0,smoothLength):
+        values[i] = values[i] * i / smoothLength;
+        values[-1-i] = values[-1-i] * i / smoothLength;
+    return values
+
+INSTRUMENTS = {'sine':sineWave, 'square':squareWave,'guitar':guitarWave}
+def waveGen(note, length, waveType, vol=1):
+    """waveGen
+        note - the wave
+        length - the length to play the wave for in milliseconds
+        waveType - what kind of wave to make. This is a string.
+        returns a string representing an 8 bit mono wave"""
+    sampleCount=(SAMPLE_RATE*length)//1000
+    cachekey=(waveType,note,sampleCount)
+    if(cachekey not in cache):
+        cache[cachekey]=INSTRUMENTS.get(waveType,DEFAULT_INSTRUMENT)(FREQS[note], sampleCount,vol)
+    return cache[cachekey]
+        
+def divide(inwave, n):
+    """divide - basically adjust the volume
+        wave - a wave string
+        n    - a number to divide by
+        returns a new wave string"""
+    l=len(wave)
+    outwave=initArray(l)
+    for i in xrange(0,l):
+        outwave[i]=inwave[i]//n
+    return outwave
+    
+def limit(n):
+    """limit
+        n - a number
+        returns a number"""
+    if (n > MAX):
+        return MAX
+    elif (n < -MAX):
+        return -MAX
+    return n
+
+def mergeWaves(w1,w2):
+    """mergeWaves - merge two waves together
+        w1 - a wave
+        w2 - a wave
+        returns a new wave that is both combined"""
+    l=max(len(w1),len(w2))
+    w1.extend([0]*(l-len(w1))) # make sure they are long enough
+    w2.extend([0]*(l-len(w2)))
+    
+    outwave=initArray(l)
+    for i in xrange(0,l):
+        outwave[i]=limit(w1[i]+w2[i]) # what todo: when one list is longer?
+    return outwave
+    #li=[schr(ord(a)+ord(b)-0x80) for (a,b) in izip_longest(wave1,wave2,fillvalue=0)]
+    #return ''.join(li)
+    
+def initArray(size=0):
+    return array.array('h',[0]*size)    
+
+def makeWavFile(data,filename):
+    """makeWave
+        data - the wave to put into the file
+        filename - the name of the file to open"""
+    f = wave.open(filename, 'w')
+    #f.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
+    # don't use mono, sound will be cut off at half
+    f.setparams((1, 2, SAMPLE_RATE, 0, 'NONE', 'not compressed'))
+    f.writeframes(data)
+    f.close()
+    
+    
+''' int smoothLength = (int) (SAMPLE_RATE * 0.005);
+        end--;
+        for (int i = 0; i < smoothLength && i < end / 2; i++) {
+            data[start + i] = (byte) (data[start + i] * i / smoothLength);
+            data[end - i] = (byte) (data[end - i] * i / smoothLength);
+        }'''
